8b354e0ac4cde03c073591a3f8bc5859c745c0ad
YongChan <yongchan.312@gmail.com>
2021-12-20 18:41:52 +0900

    first commit

---
+++ content/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
+
+
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id')
+
+        return render(request, "insta/main.html", context = dict(feeds = feed_list))
